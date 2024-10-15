11f63358bc13f192c0fc318b0a3148e78e120c37
melkisedeath <manoskaristineos@gmail.com>
2024-10-15 10:13:48 +0200

    Added installation of packages in the setup.

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
 
+try:
+    import torch
+except ImportError:
+    raise ImportError("Pytorch is not installed. Please install it using 'pip install torch'")
+
+
 setup(
     name="piano_svsep",
-    version="0.1",
+    version="0.0.1dev",
     packages=find_packages(),
+    setup_requires=["torch"],
     install_requires=[
-        "partitura",
-        "torchmetrics",
-        "scipy",
-        "pytorch_lightning",
-        "verovio",
-        "torch-scatter",
+        "pytorch-geometric==1.7.2",
+        "partitura==1.5.0",
+        "torchmetrics==0.5.0",
+        "scipy==1.14.0",
+        "pytorch_lightning==2.3.3",
+        "verovio==4.2.1",
+        "torch-scatter==2.1.2",
+        "joblib==1.0.1",
+        "gitpython==3.1.14",
+        "tqdm==4.61.0",
     ],
 )
